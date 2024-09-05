1e06f5b62eb3a8e19f1c7286de960a43ab729a8e
P0lid0 <labsfiap@fiap.com.br>
2024-09-04 21:53:14 -0300

    Primeira-Aula

---
+++ makeit/src/Header.jsx
@@ -0,0 +1,23 @@
+import "./Header.css"
+
+const Header = () => {
+    
+    return (
+        <header>
+            <nav>
+                <h1>Seja bem vindo ao nosso site</h1>
+                <ul>
+                    <li>Sobre</li>
+                    <li>contats</li>
+                </ul>
+            </nav>
+
+        </header>
+    )
+    
+    
+    
+    
+}
+
+export default Header;
